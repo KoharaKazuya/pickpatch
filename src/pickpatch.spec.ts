59091c8948a324df0ee406058d78584596402ba0
KoharaKazuya <reflect.results@gmail.com>
2018-05-27 12:00:05 +0900

    add tests

--- src/pickpatch.spec.ts
+++ src/pickpatch.spec.ts
@@ -1,36 +1,50 @@
 import pickpatch from "./pickpatch";
 
 describe("pickpatch", () => {
-  it("picks any from nested object and patch", () => {
-    const base = { a: { b: 1 }, c: 2 };
-    const picker = (obj: any) => obj.a.b;
-    const patcher = (v: number) => v * 10;
-    const updated = pickpatch(picker)(patcher)(base);
-    expect(updated).toEqual({ a: { b: 10 }, c: 2 });
+  it("updates any partial of any data", () => {
+    const dataset: Array<
+      [any, (...args: any[]) => any, (...args: any[]) => any, any]
+    > = [
+      [1, _ => _, x => x + 1, 2],
+      [1, _ => null, x => 2, 1],
+      [{ a: { b: 1 }, c: 2 }, x => x.a.b, v => v * 10, { a: { b: 10 }, c: 2 }],
+      [
+        { a: { b: 1 }, c: 2 },
+        x => [x.a.b, x.c],
+        ([b, c]) => [b * 2, c * 3],
+        { a: { b: 2 }, c: 6 },
+      ],
+      [
+        { a: { b: 1 }, c: 2 },
+        x => ({ d: x.a.b, e: [x.c] }),
+        ({ d, e }) => ({ d: d + 1, e: [(d + 10) * e[0]] }),
+        { a: { b: 2 }, c: 22 },
+      ],
+    ];
+
+    for (const d of dataset) {
+      expect(pickpatch(d[1])(d[2])(d[0])).toEqual(d[3]);
+    }
   });
 
-  it("can pick as array", () => {
-    const base = { a: { b: 1 }, c: 2 };
-    const picker = (obj: any) => [obj.a.b, obj.c] as [number, number];
-    const patcher = ([v1, v2]: [number, number]) =>
-      [v1 * 2, v2 * 3] as [number, number];
-    const updated = pickpatch(picker)(patcher)(base);
-    expect(updated).toEqual({ a: { b: 2 }, c: 6 });
+  it("does not update base object (immutability)", () => {
+    const nested = { b: 1 };
+    const base = { a: nested, c: 2 };
+    const _ = pickpatch((x: any) => [x.a.b, x.c])(([b, c]) => [b + 1, c + 1])(
+      base,
+    );
+
+    expect(base).toEqual({ a: { b: 1 }, c: 2 });
+    expect(base.a).toBe(nested);
   });
 
-  it("picks as any form", () => {
-    const base = { a: { b: 1 }, c: 2 };
-    const picker = (obj: any) => ({ d: obj.a.b, e: [obj.c] });
-    const patcher = ({ d, e }: any) => ({ d: d + 1, e: [(d + 10) * e[0]] });
-    const updated = pickpatch(picker)(patcher)(base);
-    expect(updated).toEqual({ a: { b: 2 }, c: 22 });
-  });
+  it("returns the same object unless any update", () => {
+    const base = { a: 1 };
+    expect(pickpatch(_ => null)(_ => null)(base)).toBe(base);
 
-  it("can pick as null", () => {
-    const base = { a: { b: 1 }, c: 2 };
-    const picker = (obj: any) => null;
-    const patcher = () => null;
-    const updated = pickpatch(picker)(patcher)(base);
-    expect(updated).toEqual({ a: { b: 1 }, c: 2 });
+    const tree = { a: { b: 2 }, c: { d: 3 } };
+    const updated = pickpatch((x: any) => x.a.b)(v => v * 2)(tree);
+    expect(updated.a).not.toBe(tree.a);
+    expect(updated.c).toBe(tree.c);
   });
 });
